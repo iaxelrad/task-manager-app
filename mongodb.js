80b9f6d5a9c4d3cb57d4c3e917f5233ad7e4a196
Itamar Axelrad <itamara@aman.co.il>
2021-09-08 21:30:18 +0300

    Inserting many documents.

--- mongodb.js
+++ mongodb.js
@@ -19,9 +19,47 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Itamar',
-      age: 31,
-    });
+    // db.collection('users').insertOne(
+    //   {
+    //     name: 'Itamar',
+    //     age: 31,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany(
+    //   [
+    //     { name: 'Jen', age: 28 },
+    //     { name: 'Gunther', age: 27 },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert documents');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection('tasks').insertMany(
+      [
+        { description: 'Installed mongoDB on macbook', completed: true },
+        { description: 'Set up the new task manager app', completed: true },
+        { description: 'Finished udemy node.js course', completed: false },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert documents');
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
