7aa731a8a0f6c4ca8be8ed95690b77a845646197
Itamar Axelrad <itamara@aman.co.il>
2021-09-09 22:53:10 +0300

    Data validation and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -6,16 +7,50 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useUnifiedTopology: true,
 });
 
-// const User = mongoose.model('User', {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (validator.matches(value.toLowerCase(), 'password')) {
+        throw new Error("Password can't contain 'password'!");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    },
+  },
+});
 
-// const me = new User({ name: 'Itamar', age: 31 });
+// const me = new User({
+//   name: '    Yonatan    ',
+//   email: 'yoNaTan@test.coM',
+//   password: '   pass   ',
+// });
 
 // me.save()
 //   .then(() => {
@@ -28,13 +63,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const Task = mongoose.model('Task', {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
-const task = new Task({ description: 'Do the first Task instance', completed: true });
+const task = new Task({ description: '    Do the third Task instance    ', completed: true });
 
 task
   .save()
