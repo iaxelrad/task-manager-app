70903573c628f5e283e757a130f4ec4e781e3b6c
Itamar Axelrad <itamara@aman.co.il>
2021-09-10 09:15:37 +0300

    Some Async await examples

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,6 +1,5 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
-const Task = require('../src/models/task');
 
 // // 613a6f85f89ab755aeba8a6f
 // User.findByIdAndUpdate('613919d1037da404388ea04e', { age: 1 })
@@ -13,15 +12,12 @@ const Task = require('../src/models/task');
 //   })
 //   .catch(e => console.log(e));
 
-//   const User = require('../src/models/user');
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const userCount = await User.countDocuments({ age });
+  return userCount;
+};
 
-// 613a6f85f89ab755aeba8a6f
-Task.findByIdAndDelete('613a7207fde149570f81dd04')
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then(userCount => {
-    console.log(userCount);
-  })
+updateAgeAndCount('613919d1037da404388ea04e', 3)
+  .then(userCount => console.log(userCount))
   .catch(e => console.log(e));
