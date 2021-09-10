5dc2c3cb4d58532cca53ceaadcb9292b58fdca0a
Itamar Axelrad <itamara@aman.co.il>
2021-09-10 10:29:18 +0300

    Resource Updating Endpoints

--- src/index.js
+++ src/index.js
@@ -8,80 +8,119 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then(users => {
-      res.send(users);
-    })
-    .catch(err => {
-      res.status(500).send();
-    });
-});
-
-app.get('/users/:id', (req, res) => {
-  const _id = req.params.id;
-
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send();
-      }
-
-      res.send(user);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
-});
-
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.send(user);
-    })
-    .catch(err => {
-      res.status(400).send(err);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.send(tasks);
-    })
-    .catch(err => {
-      res.status(500).send();
-    });
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
 
-  Task.findById(_id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send();
-      }
-
-      res.send(task);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
-app.post('/tasks', (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch(err => {
-      res.status(400).send(err);
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(404).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
     });
+    if (!updatedUser) {
+      return res.status(404).send();
+    }
+
+    res.send(updatedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(404).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedTask) {
+      return res.status(404).send();
+    }
+
+    res.send(updatedTask);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 app.listen(port, () => console.log(`Server is up on ${port}`));
