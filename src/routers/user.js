51d54396819f62a1366817f7464e00c13c31ce34
Itamar Axelrad <itamara@aman.co.il>
2021-09-13 22:22:22 +0300

    Validating File Uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -83,6 +83,16 @@ router.delete('/users/me', auth, async (req, res) => {
 
 const upload = multer({
   dest: 'avatars',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please provide an image file'));
+    }
+
+    cb(undefined, true);
+  },
 });
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
