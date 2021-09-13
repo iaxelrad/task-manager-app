2b8b47def0b1f89acbeb959375ea72cc6d8b715f
Itamar Axelrad <itamara@aman.co.il>
2021-09-14 00:06:04 +0300

    Sending Welcome and Cancelation Emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,18 +1,23 @@
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'iaxelrad@gmail.com', // Change to your recipient
-  from: 'iaxelrad@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent');
-  })
-  .catch(error => {
-    console.error(error.body.errors);
+sgMail.setApiKey('YOUR_API_KEY');
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'iaxelrad@gmail.com',
+    subject: 'Welcome to the app',
+    text: `Welcome to the app ${name}, let me know how you like the app.`,
   });
+};
+
+const sendCancelEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'iaxelrad@gmail.com',
+    subject: 'Why so soon?',
+    text: `Sorry to see you leave ${name}, You are always welcome back.`,
+  });
+};
+
+module.exports = { sendWelcomeEmail, sendCancelEmail };
