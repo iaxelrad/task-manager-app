015f55c542e893f40ee69144b672e5168a18e15a
Itamar Axelrad <itamara@aman.co.il>
2021-09-15 19:33:39 +0300

    Advanced Assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -22,7 +22,7 @@ beforeEach(async () => {
 });
 
 test('should signup a new user', async () => {
-  await request(app)
+  const response = await request(app)
     .post('/users')
     .send({
       name: 'Itamar',
@@ -30,13 +30,27 @@ test('should signup a new user', async () => {
       password: 'MyPass777!',
     })
     .expect(201);
+
+  //Assert that the database has been changed correctly
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  //Assertion about the response
+  expect(response.body).toMatchObject({
+    user: { name: 'Itamar', email: 'itamar3@test.com' },
+    token: user.tokens[0].token,
+  });
+  expect(user.password).not.toBe('MyPass777!');
 });
 
 test('should login existing user', async () => {
-  await request(app).post('/users/login').send({
+  const response = await request(app).post('/users/login').send({
     email: userOne.email,
     password: userOne.password,
   });
+
+  const user = await User.findById(userOneId);
+  expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test('should not login nonexistent user', async () => {
@@ -58,10 +72,7 @@ test('should fetch profile for user', async () => {
 });
 
 test('should not get profile for unauthenticated user', async () => {
-  await request(app)
-    .get('/users/me')
-    .send()
-    .expect(401);
+  await request(app).get('/users/me').send().expect(401);
 });
 
 test('should delete a user', async () => {
@@ -70,12 +81,10 @@ test('should delete a user', async () => {
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
+  const user = await User.findById(userOneId);
+  expect(user).toBeNull();
 });
 
 test('should not delete user for unauthenticated user', async () => {
-  await request(app)
-    .delete('/users/me')
-    .send()
-    .expect(401);
+  await request(app).delete('/users/me').send().expect(401);
 });
-
